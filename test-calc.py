36a1b525be3cbadd4d94651322ac18b2fd1e5d1e
uzairpatel2001 <uzairpatel2001@gmail.com>
2025-10-26 12:17:56 +0530

    test 1

---
+++ test-calc.py
@@ -0,0 +1,15 @@
+import pytest
+from calc import add,sub,mul,div
+
+def test_add():
+    # compare actual o/p and expected o/p
+    assert add(2,4) == 6
+
+def test_sub():
+    assert sub(2,1) == 1
+
+def test_sub():
+    assert mul(3,2) == 6
+
+def test_div():
+    assert div(4/2) == 2
